b87b2862095f741889b2e9ae2f117e83b3fa3074
Jake Cataford <jakecataford@gmail.com>
2014-06-26 10:58:31 -0400

    Macports path

--- bash/path.sh
+++ bash/path.sh
@@ -2,4 +2,5 @@ export PATH=/usr/local/sbin:$PATH
 export PATH=/usr/local/bin:$PATH
 export PATH=~/.rbenv/bin:$PATH
 export PATH=~/Lib/android-sdk/platform-tools:$PATH
+export PATH=/opt/local/bin:/opt/local/sbin:$PATH
 eval "$(rbenv init -)"
